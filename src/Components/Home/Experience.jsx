ff5507a579905e1c1e8bb05a2734874a4cb09565
Palak Patel <114860110+PalakPatel-04@users.noreply.github.com>
2025-02-27 01:17:00 +0530

    Add files via upload

---
+++ src/Components/Home/Experience.jsx
@@ -0,0 +1,127 @@
+import Entry from "./Entry/Entry";
+
+const Experience = ({ items }) => {
+  const programming = [
+    {
+      name: "C++",
+      level: "Intermediate",
+    },
+    {
+      name: "JavaScript",
+      level: "Intermediate",
+    },
+    {
+      name: "Python",
+      level: "Basic",
+    },
+    {
+      name: "SQL",
+      level: "Intermediate",
+    },
+  ];
+  const frontend = [
+    {
+      name: "HTML"
+    },
+    {
+      name: "CSS"
+    },
+    {
+      name: "Bootstrap"
+    },
+    {
+      name: "Tail WindCSS"
+    },
+    {
+      name: "React"
+    },
+  ];
+  const backend = [
+    {
+      name: "Node.Js"
+    },
+    {
+      name: "Express.Js"
+    },
+    {
+      name: "MongoDB"
+    },
+  ];
+  const tools = [
+    {
+      name: "VS Code"
+    },
+    {
+      name: "Github"
+    },
+    {
+      name: "Git"
+    },
+    {
+      name: "Vercel/ Netlify"
+    },
+    {
+      name: "Render"
+    },
+    {
+      name: "Jupyter"
+    }
+  ];
+  return (
+    <section id="experience" data-aos="slide-up">
+      <p className="section__text__p1">Explore My</p>
+      <h1 className="title">Experience</h1>
+      <div className="experience-details-container" data-aos="slide-up">
+        <div className="about-containers-experience">
+
+          <div className="details-container" data-aos="slide-up">
+            <h2 className="experience-sub-title">
+              Programming languages
+            </h2>
+            <div className="article-container">
+              <article>
+                {programming.map((item) => (
+                  <Entry name={item.name} level={item.level}></Entry>
+                ))}
+              </article>
+            </div>
+          </div>
+
+          
+          <div className="details-container" data-aos="slide-up">
+            <h2 className="experience-sub-title">Front End Development</h2>
+            <div className="article-container">
+              <article>
+                {frontend.map((item) => (
+                  <Entry name={item.name} level={item.level}></Entry>
+                ))}
+              </article>
+            </div>
+          </div>
+          <div className="details-container" data-aos="slide-up">
+            <h2 className="experience-sub-title">Back End Development</h2>
+            <div className="article-container">
+              <article>
+                {backend.map((item) => (
+                  <Entry name={item.name} level={item.level}></Entry>
+                ))}
+              </article>
+            </div>
+          </div>
+          <div className="details-container" data-aos="slide-up">
+            <h2 className="experience-sub-title">Tools</h2>
+            <div className="article-container">
+              <article>
+                {tools.map((item) => (
+                  <Entry name={item.name} level={item.level}></Entry>
+                ))}
+              </article>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Experience;
---
+++ src/Components/Project/DetailedProjects.jsx
@@ -0,0 +1,155 @@
+import ProjectContainer from "./ProjectContainer";
+
+const DetailedProject = ({ setSelectedTab }) => {
+  const projectContainer = [
+    {
+      number: 1,
+      title: "Blood donor connect portal",
+      description:
+        "The Blood Donor Connect Portal is a full-stack web application built using the MERN stack, designed to connect blood donors with recipients. The platform allows users to register, search for donors, and update their profiles. Used RESTful APIs for data exchange and integrated JWT authentication to secure user and admin logins.",
+      button1: "Github",
+      button1link: "https://github.com/PalakPatel-04/Blood-Donor-Connect-Portal-",
+      button2: "Live Demo",
+      // button2link: "#",
+      imagelink: "./assets/blood-donate.jpg",
+    },
+    {
+      number: 2,
+      title: "MediLocate",
+      description:
+        "MediLocate, a smart healthcare assistant that helps users find nearby hospitals based on their real-time location.",
+      button1: "Github",
+      button1link: "https://github.com/PalakPatel-04/MediLocate",
+      button2: "Live Demo",
+      button2link: "https://medilocate-omega.vercel.app/login",
+      imagelink: "./assets/medloc.png",
+    },
+    {
+      number: 3,
+      title: "Portfolio",
+      description:
+        "The Portfolio is a personal project that I developed to showcase my skills, accomplishments, and the projects I have worked on.",
+      button1: "Github",
+      button1link: "#",
+      button2: "Live Demo",
+      button2link: "#",
+      imagelink: "./assets/portfolio.jpg",
+    },
+  ];
+
+  const miniProject = [
+    {
+      number: 2,
+      title: "Trip2Go",
+      description:
+        "Trip2Go is a web application designed to simplify the process of planning your next trip.",
+      button1: "Github",
+      button1link: "https://github.com/PalakPatel-04/Trip2Go",
+      button2: "Live Demo",
+      button2link: "https://trip2go.vercel.app/",
+      imagelink: "./assets/triptogo.jpg",
+    },
+    {
+      number: 3,
+      title: "QuickBite",
+      description:
+        "QuickBite is a modern reservation management platform designed to simplify the process of booking and managing reservations. ",
+      button1: "Github",
+      button1link: "https://github.com/PalakPatel-04/QuickBite",
+      button2: "Live Demo",
+      button2link: "https://quickbite-eight.vercel.app/",
+      imagelink: "./assets/quickbite.jpeg",
+    },
+    {
+      number: 4,
+      title: "LangDetect",
+      description:
+        "LangDetect is a Python-based language detection project that utilizes pandas for data handling and scikit-learn for feature extraction. Also Multinomial Naive Bayes Classifier to accurately classify text input by language.",
+      button1: "Github",
+      button1link: "https://github.com/PalakPatel-04/LangDetect",
+      button2: "GitHub Demo",
+      button2link: "https://github.com/PalakPatel-04/LangDetect",
+      imagelink: "./assets/langdetect.png",
+    },
+    {
+      number: 5,
+      title: "ShopEase",
+      description:
+        "ShopEase, an eCommerce website developed using basic HTML and CSS.",
+      button1: "Github",
+      button1link: "https://github.com/PalakPatel-04/ShopEase",
+      button2: "Live Demo",
+      button2link: "https://shop-ease-online.vercel.app/",
+      imagelink: "./assets/shopease.png",
+    },
+  ];
+
+  return (
+    <>
+      <section
+        id="aboutpart a"
+        className="about"
+        data-aos="slide-up"
+        style={{
+          textAlign: "center",
+          margin: "auto",
+          padding: "20px 40px", 
+          maxWidth: "1200px", 
+          backgroundColor: "#f9f9f9",
+          borderRadius: "10px",
+          boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)", 
+        }}
+      >
+        <div className="back-btn-container">
+        <button
+            className="btn btn-color-2"
+            onClick={() => {
+              setSelectedTab("Home");
+            }}
+          >
+            &#8617; Home
+          </button>
+        </div>
+        <h1 data-aos="slide-up" className="title">
+          My Projects
+        </h1>
+        <br />
+        <div style={{ display: "flex", flexDirection: "column", gap: "20px" }}>
+          {projectContainer.map((item) => (
+            <ProjectContainer
+              key={item.number}
+              number={item.number}
+              title={item.title}
+              description={item.description}
+              button1={item.button1}
+              button1link={item.button1link}
+              button2={item.button2}
+              button2link={item.button2link}
+              imagelink={item.imagelink}
+            />
+          ))}
+        </div>
+        <br />
+        <br />
+        <h3 data-aos="slide-up">Mini/Side Projects</h3>
+        <div style={{ display: "flex", flexDirection: "column", gap: "20px" }}>
+          {miniProject.map((item) => (
+            <ProjectContainer
+              key={item.number}
+              number={item.number}
+              title={item.title}
+              description={item.description}
+              button1={item.button1}
+              button1link={item.button1link}
+              button2={item.button2}
+              button2link={item.button2link}
+              imagelink={item.imagelink}
+            />
+          ))}
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default DetailedProject;
